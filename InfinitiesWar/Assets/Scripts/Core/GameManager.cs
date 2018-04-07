940a139a6d4384e9f4a8cd4547165ccae1c62a9f
Данил <redreev.danil@yandex.ru>
2018-04-07 14:29:06 +0500

    First steps

---
+++ InfinitiesWar/Assets/Scripts/Core/GameManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+    public string PlayerName { get; private set; }
+
+    public void SetPlayerName(string name)
+    {
+        PlayerName = name;
+    }
+}
---
+++ InfinitiesWar/Assets/Scripts/UI/MainMenuBehaviour.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuBehaviour : MonoBehaviour {
+
+
+    public void StartGame()
+    {
+
+    }
+
+}
