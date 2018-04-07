46102c95990d0eba40e2f28d42f5e28a7867157f
Данил <redreev.danil@yandex.ru>
2018-04-07 14:42:05 +0500

    Main scene

--- InfinitiesWar/Assets/Scripts/Core/GameManager.cs
+++ InfinitiesWar/Assets/Scripts/Core/GameManager.cs
@@ -3,10 +3,22 @@ using System.Collections;
 
 public class GameManager : MonoBehaviour {
 
+    static GameManager gameManager;
+
     public string PlayerName { get; private set; }
 
     public void SetPlayerName(string name)
     {
         PlayerName = name;
+    }
+
+    private void Awake()
+    {
+        if (gameManager != null)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+
+        gameManager = this;
     }
 }
--- InfinitiesWar/Assets/Scripts/UI/MainMenuBehaviour.cs
+++ InfinitiesWar/Assets/Scripts/UI/MainMenuBehaviour.cs
@@ -1,12 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuBehaviour : MonoBehaviour {
 
+    [SerializeField]
+    InputField playerNameInput;
 
     public void StartGame()
     {
+        var manager = FindObjectOfType<GameManager>();
 
+        var playerName = playerNameInput.text;
+        manager.SetPlayerName(playerName);
+
+        SceneManager.LoadScene("Game");
     }
 
 }
