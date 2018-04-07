f6ef246373aea6e9a48cd08b1acf80297a7adb16
malaev <potap99122@gmail.com>
2018-04-07 17:56:24 +0500

    add Enemy

---
+++ InfinitiesWar/Assets/Scripts/Game/EnemyActions.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyActions : MonoBehaviour {
+
+    [SerializeField]
+    TextMesh Enemy;
+
+    public int CurrentNumber;
+
+    // Use this for initialization
+    void Start()
+    {
+        CurrentNumber = (int)Random.Range(1, 9);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Enemy.text = CurrentNumber.ToString();
+    }
+}
