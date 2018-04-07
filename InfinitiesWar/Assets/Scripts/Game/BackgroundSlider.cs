2fb643c9ed74afaa1f8f6819300bec05e26f0103
Данил <redreev.danil@yandex.ru>
2018-04-07 15:32:48 +0500

    BackgroundSlider

---
+++ InfinitiesWar/Assets/Scripts/Game/BackgroundSlider.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundSlider : MonoBehaviour {
+
+    [SerializeField]
+    Transform leftBound;
+    [SerializeField]
+    Transform rightBound;
+
+    [SerializeField]
+    float speed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position -= new Vector3(speed, 0, 0);
+
+        if(transform.position.x < leftBound.position.x)
+        {
+            transform.position = new Vector3(rightBound.position.x - leftBound.transform.position.x + transform.position.x, transform.position.y, transform.position.z);
+        }
+	}
+}
