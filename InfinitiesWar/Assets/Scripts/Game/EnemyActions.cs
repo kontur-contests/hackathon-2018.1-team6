57008360a96b28d7936d6e6d24bdb64b74af8d67
malaev <potap99122@gmail.com>
2018-04-08 05:02:58 +0500

    Gratest Algo

--- InfinitiesWar/Assets/Scripts/Game/EnemyActions.cs
+++ InfinitiesWar/Assets/Scripts/Game/EnemyActions.cs
@@ -46,13 +46,43 @@ public class EnemyActions : MonoBehaviour {
 
     private int GetValideNumber(int curr)
     {
+        var curQueue = World.world.numbersController.enemies;
+        var copy = new Queue<EnemyActions>(curQueue);
+        copy.ToArray();
         var CNums = new List<int>();
+        bool[,] d = new bool[copy.Count, 10];
+        for (var i = 0; i < copy.Count; ++i)
+            for (var j = 0; j < 10; ++j)
+                d[i, j] = false;
+        foreach (var elem in ValideNumbers(curr))
+        {
+            d[0, elem] = true;
+        }
+        for (var i = 1; i < copy.Count; ++i)
+        {
+            for (var j = 1; j < 10; ++j)
+                if (d[i - 1, j])
+                    foreach (var elem in ValideNumbers(j))
+                        d[i, elem] = true;
+        }
+        var answer = new List<int>();
+        for (var i = 1; i < 10; ++i)
+        {
+            if (d[copy.Count - 1, i])
+                answer.Add(i);
+        }
+        return answer[(int)Random.Range(0, answer.Count)];
+    }
+
+    private List<int> ValideNumbers(int curr)
+    {
+        var valideNumbers = new List<int>();
         for (int i = 1; i < 10; ++i)
         {
             if (isValid(i, curr))
-                CNums.Add(i);
+                valideNumbers.Add(i);
         }
-        return CNums[(int)Random.Range(0, CNums.Count)];
+        return valideNumbers;
     }
 
     private bool isValid(int i, int curr)
